fd3728139dbe783fd5510ed8915dec938c158a3d
AKBasit <141636098+AKBasit@users.noreply.github.com>
2024-02-03 12:26:40 +0000

    Finished

--- src/index.js
+++ src/index.js
@@ -27,6 +27,8 @@ const cards = [
 
 const memoryGame = new MemoryGame(cards);
 
+memoryGame.shuffleCards()
+
 window.addEventListener('load', (event) => {
   let html = '';
   memoryGame.cards.forEach((pic) => {
@@ -41,11 +43,47 @@ window.addEventListener('load', (event) => {
   // Add all the divs to the HTML
   document.querySelector('#memory-board').innerHTML = html;
 
-  // Bind the click event of each element to a function
-  document.querySelectorAll('.card').forEach((card) => {
-    card.addEventListener('click', () => {
-      // TODO: write some code here
-      console.log(`Card clicked: ${card}`);
-    });
-  });
-});
+  // Selecting the element to update
+
+  let pairsClicked = document.getElementById("pairs-clicked")
+  let pairsGuessed = document.getElementById("pairs-guessed")
+
+  let cardCounter = 0
+
+  document.querySelectorAll(".card").forEach((card) => {
+    card.addEventListener("click", () => {
+      card.classList("click", () => {
+        memoryGame.pickedCards.push(card)
+        cardCounter++
+        if (cardCounter === 2) {
+          
+        }
+      })
+    })
+  })
+
+
+//   // Bind the click event of each element to a function
+
+// document.querySelectorAll(".card").forEach((card) => {
+//   card.addEventListener("click", () => {
+//     card.classList.toggle("turned", true)
+//     memoryGame.pickedCards
+//   })
+// })
+
+
+//   document.querySelectorAll('.card').forEach((card) => {
+//     card.addEventListener('click', () => {
+//       card.classList.toggle("turned", true)
+//       memoryGame.pickedCards.push(card)
+      
+//       // card counter++
+//       if(memoryGame.pickedCards.length === 2) {
+//         let card1 = memoryGame.pickedCards[0].getAttribute
+//       }
+//       // TODO: write some code here
+//       console.log(`Card clicked: ${card}`);
+//     });
+//   });
+// });
